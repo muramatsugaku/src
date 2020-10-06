47899d88cbf0d65b858456ac8a034d77df196e00
504a <moritaka@wdc-kd.com>
2020-10-06 15:02:27 +0900

    20201006

--- Q2.java
+++ Q2.java
@@ -1,4 +1,7 @@
 package question;
+
+import java.util.Scanner;
+
 /*
     身長と体重を入力してBMIを求める
     BMIの求め方
@@ -6,6 +9,29 @@ package question;
  */
 public class Q2 {
     public static void main(String[] args) {
+        //入力の準備
+        Scanner sc = new Scanner(System.in);
 
+        //入力処理
+        System.out.println("身長を入力してください(m)");
+        double height = sc.nextDouble();
+        System.out.println("体重を入力してください(kg)");
+        double weight = sc.nextDouble();
+
+        //計算と結果表示
+        //Mathクラスのpow()メソッド
+        //https://docs.oracle.com/javase/jp/11/docs/api/java.base/java/lang/Math.html#pow(double,double)
+        System.out.println("身長：" + height * 100 + "cm 体重：" + weight + "kg");
+        System.out.println("BMI:" +  weight / Math.pow(height,2));
+        System.out.println("適性体重：" + (Math.pow(height,2) * 22) + "kg");
+        System.out.println("現在との差(" + (weight - Math.pow(height,2) * 22) + "kg)");
+
+        //参考
+        //書式付き書式付き文字列
+        //https://docs.oracle.com/javase/jp/11/docs/api/java.base/java/lang/String.html#format(java.lang.String,java.lang.Object...)
+        System.out.println(String.format("\n身長：%.2fm　体重：%.2fkg",height * 100,weight ));
+        System.out.println(String.format("BMI:%.2f",weight / Math.pow(height,2)));
+        System.out.println(String.format("適性体重：%.2fkg",Math.pow(height,2) * 22));
+        System.out.println(String.format("現在との差(%.2fkg)",weight - Math.pow(height,2) * 22));
     }
 }

368ce6f571ec42f0c32dfef6e39789d43d4e3706
504a <moritaka@wdc-kd.com>
2020-10-09 12:41:15 +0900

    20201009

---
+++ If1.java
@@ -0,0 +1,23 @@
+package ex1;
+
+import java.util.Scanner;
+
+public class If1 {
+    public static void main(String[] args) {
+        //if命令の基本
+        //例　入力された数値が決められた範囲内かを調べる
+        Scanner sc = new Scanner(System.in);
+        int min = 1,max = 100;
+        System.out.println("入力値が" + min + "~" + max + "の範囲かどうか調べます");
+        System.out.println("数値を入力してください");
+        int num = sc.nextInt();
+
+        if(num >= min && num <= max) {
+            //条件がtrueの場合
+            System.out.println("入力値:" + num + "は" + min + "~" + max + "の範囲内です");
+        } else {
+            //条件がfalseの場合
+            System.out.println("入力値:" + num + "は" + min + "~" + max + "の範囲外です");
+        }
+    }
+}

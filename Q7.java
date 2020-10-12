535b6809bdf61d6a4d352cef6eeec8d7aa25bba2
504a <moritaka@wdc-kd.com>
2020-10-12 12:27:55 +0900

    20201012

---
+++ Q7.java
@@ -0,0 +1,16 @@
+package question;
+
+public class Q7 {
+    public static void main(String[] args) {
+        //テストの得点データがある
+        double[] score = {60,75,24,88,43,100,32,50,18,94};
+
+        //平均点を求めなさい
+
+        //60点以上を合格とした場合
+        //合格者の人数を求めなさい
+
+        //合格者を対象とした平均点を求めなさい
+        
+    }
+}

e10fa973c028c28c07e732601b2549bf64badd35
504a <moritaka@wdc-kd.com>
2020-10-13 14:27:35 +0900

    20201013

---
+++ Array2.java
@@ -0,0 +1,25 @@
+package ex1;
+
+import java.util.Arrays;
+
+public class Array2 {
+    public static void main(String[] args) {
+        //配列を扱う
+        int[] array = {40,10,30,50,20};
+        System.out.println(Arrays.toString(array));
+        System.out.println(array);//ハッシュ値が表示される
+
+        //配列をコピーする
+        int[] array2 = Arrays.copyOf(array,10);
+        System.out.println(Arrays.toString(array2));
+
+        //配列をソートする
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+        //バイナリーサーチ
+        //見つかった場所のインデックス
+        System.out.println(Arrays.binarySearch(array,30));//2
+        System.out.println(Arrays.binarySearch(array,0));//-1
+    }
+}
---
+++ Q10.java
@@ -0,0 +1,17 @@
+package question;
+
+public class Q10 {
+    public static void main(String[] args) {
+        /*
+            同じデータが複数ある配列のサーチ
+            https://docs.oracle.com/javase/jp/11/docs/api/java.base/java/util/Arrays.html#binarySearch(int%5B%5D,int,int,int)
+            下記の配列から値が０のインデックスをすべて表示しなさい
+            表示例
+            0
+            2
+            4
+         */
+
+        int[] array = {0,10,0,50,0};
+    }
+}

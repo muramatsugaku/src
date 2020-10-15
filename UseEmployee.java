85d4dcdef5c047c5cc409cf994b8e621cbb181b0
504a <moritaka@wdc-kd.com>
2020-10-15 09:56:45 +0900

    20201015

--- UseEmployee.java
+++ UseEmployee.java
@@ -3,10 +3,8 @@ package ex2;
 public class UseEmployee {
     public static void main(String[] args) {
         //インスタンスを作る
-        Employee e1 = new Employee(1,"あいざわ","男",55);
-        System.out.println(e1.getId());//1
-        System.out.println(e1.getName());//あいざわ
-        System.out.println(e1.getGender());//男
-        System.out.println(e1.getAge());//55
+        Employee e1 = new Employee(1,"あいざわ","男",55,100,400,350);
+
+        System.out.println(e1);
     }
 }

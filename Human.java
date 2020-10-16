cb5b4330ffd88707184d4f53ff6d0e32690bb8de
504a <moritaka@wdc-kd.com>
2020-10-16 09:49:57 +0900

    20201016

---
+++ Human.java
@@ -0,0 +1,17 @@
+package question.q11;
+/*
+    Humanクラスを継承し、hello()メソッドをそれぞれの国の言語に変更したクラスを作成しなさい
+    作成クラス Japanese,French,Chinese
+    実行クラス Q11 4つのクラスのインスタンスを作成して挨拶を表示しなさい
+ */
+class Human {
+    protected String name;
+
+    public Human(String name) {
+        this.name = name;
+    }
+
+    public void hello() {
+        System.out.println("Hello.My name is " + name);
+    }
+}

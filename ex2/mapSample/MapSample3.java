35ffc5109a8200882183735551071d614e23b7da
504a <moritaka@wdc-kd.com>
2020-10-21 13:34:09 +0900

    20201021

---
+++ ex2/mapSample/MapSample3.java
@@ -0,0 +1,36 @@
+package ex2.mapSample;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class MapSample3 {
+
+    static void memberToMap(Map<Integer,Integer> map,List<Member> memberList) {
+        for (Member m:memberList) {
+            map.put(m.getId(),m);
+        }
+    }
+
+    public static void main(String[] args) {
+        Map<Integer,Member> memberMap = new HashMap<>();
+        List<Member> memberList = new ArrayList<>();
+        Collections.addAll(
+                memberList,
+                new Member(1,"やまだ"),
+                new Member(2,"たけだ"),
+                new Member(3,"しまだ")
+        );
+
+        memberToMap(memberMap,memberList);
+
+        if (memberMap.containsKey(1)) {
+            //id:1が存在した
+            System.out.println(memberMap.get(1));
+        } else {
+            System.out.println("そのIDは居ません");
+        }
+    }
+}

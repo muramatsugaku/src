046812d32b73d9f407ebb394731c7304412fcdfa
504a <moritaka@wdc-kd.com>
2020-10-26 15:04:08 +0900

    20201026

--- question/q19/Q19.java
+++ question/q19/Q19.java
@@ -1,8 +1,5 @@
 package question.q19;
 
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;

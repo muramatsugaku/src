1da35f313e84ed81ee4ae7181b7c287c4df42778
504a <moritaka@wdc-kd.com>
2020-10-27 10:52:45 +0900

    20201027

--- ex2/stringSample/MappingSample.java
+++ ex2/stringSample/MappingSample.java
@@ -1,7 +1,13 @@
 package ex2.stringSample;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+/**
+ * 今回のデータクラス
+ */
 class Data {
     private String name;
     private String gender;
@@ -33,6 +39,7 @@ class Data {
 
 class MappingSample {
     public static void main(String[] args) {
+        //文字列の元となるデータ
         List<String> strData = List.of(
                 "かとう,男,28",
                 "きむら,女,31",
@@ -41,6 +48,24 @@ class MappingSample {
                 "こばやし,女,24"
         );
 
+        //Dataクラスのリスト
+        List<Data> dataList = new ArrayList<>();
+
+        //変換
+        for (String str:strData) {
+            String[] dataArray = str.split(",",-1);
+            Data data = new Data(
+                    dataArray[0],//name
+                    dataArray[1],//gender
+                    Integer.valueOf(dataArray[2])//age
+            );
+            dataList.add(data);
+        }
+
+        //年齢の降順で表示
+        dataList.sort(Collections.reverseOrder(Comparator.comparing(Data::getAge)));
+        dataList.forEach(System.out::println);
+        //System.out.printf("こんにちは。私の名前は%sです。\n","やまだ");
 
     }
 }

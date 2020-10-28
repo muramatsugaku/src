c4131113d3e56f9440f46f15beb86b4c275b8616
504a <moritaka@wdc-kd.com>
2020-10-28 10:55:07 +0900

    20201028

--- ex2/dateTimeSample/DateTimeFormatSample.java
+++ ex2/dateTimeSample/DateTimeFormatSample.java
@@ -9,24 +9,24 @@ import java.util.stream.IntStream;
 class DateTimeFormatSample {
     public static void main(String[] args) {
         //日付書式のサンプル
-        final String pattern ="yyyy/MM/dd(E)";
+        final String PATTERN ="yyyy/MM/dd(E)";
         IntStream.range(1,13)
                 .mapToObj(i -> LocalDate.of(2020,i,1))
-                .map(d -> DateTimeFormatter.ofPattern(pattern).format(d))
+                .map(d -> DateTimeFormatter.ofPattern(PATTERN).format(d))
                 .forEach(System.out::println);
 
         //時刻書式のサンプル
-        final String timePattern = "ahh:mm:ss";
+        final String TIME_PATTERN = "aKK:mm:ss";
         IntStream.range(0,24)
                 .mapToObj(i -> LocalTime.of(i,00,00))
-                .map(t -> DateTimeFormatter.ofPattern(timePattern).format(t))
+                .map(t -> DateTimeFormatter.ofPattern(TIME_PATTERN).format(t))
                 .forEach(System.out::println);
 
         //和暦のサンプル
-        final String patternJ = "Gy年M月d日(EEEE)";
+        final String JAPANESE_PATTERN = "Gy年M月d日(EEEE)";
         IntStream.range(1,13)
                 .mapToObj(i -> JapaneseDate.of(1989,i,1))
-                .map(d -> DateTimeFormatter.ofPattern(patternJ).format(d))
+                .map(d -> DateTimeFormatter.ofPattern(JAPANESE_PATTERN).format(d))
                 .forEach(System.out::println);
     }
 }
--- ex2/dateTimeSample/DateTimeSample2.java
+++ ex2/dateTimeSample/DateTimeSample2.java
@@ -10,7 +10,7 @@ class DateTimeSample2 {
         System.out.println("月:" + localDateTime.getMonth());
         System.out.println("月:" + localDateTime.getMonthValue());
         System.out.println("日:" + localDateTime.getDayOfMonth());
-        System.out.println("曜日" + localDateTime.getDayOfWeek());
+        System.out.println("曜日:" + localDateTime.getDayOfWeek());
         System.out.println("時:" + localDateTime.getHour());
         System.out.println("分:" + localDateTime.getMinute());
         System.out.println("秒:" + localDateTime.getSecond());
